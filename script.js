dc1259d40303109fa48aebdd1cf8f97feded6b55
unknown <digitone@naver.com>
2020-11-26 16:39:01 +0900

    enter animation

--- script.js
+++ script.js
@@ -9,22 +9,16 @@ $(document).ready(function () {
     navigationPosition: 'right',
 
     //methods
+    // 페이지 진입시
     afterLoad: function (anchorLink, index) {
       console.log(index);
       if(index == 3) {
-        $('#thirdPage > div').append('<p>reached page</p>');
-      }
+        $('#section3 > div').append('<p>원대한 꿈에 도약합니다!</p>');
+        $('#rocket').addClass('play');
+      } else { $('#rocket').removeClass('play'); }
     }
   });
 
 });
-
-// new fullpage('#fullpage', {
-//   sectionsColor: ['gold', '#1abc9c', '#C0C0C0'],
-//   anchors:['firstPage', 'secondPage', 'thirdPage'],
-//   menu: '#myMenu',
-//   navigation: true,
-//   navigationTooltips: ['firstPage', 'secondPage', 'thirdPage'],
-// });
 
 
